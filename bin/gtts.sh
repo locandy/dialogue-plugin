a10cba815cf1a94981ecd4992dff382baf15b8be
Thomas K <thomas.katzlberger@locandy.com>
2020-01-23 21:03:41 +0100

    gtts encode audio better than Google

--- bin/gtts.sh
+++ bin/gtts.sh
@@ -26,4 +26,7 @@ base64 -D -i $2.base64 -o $2.wav
 
 rm $2.base64
 
+# Google's MP3 encodinbg sucks (ringing), we have to do it ourselves
+ffmpeg -i $2.wav -vn -ac 2 -aq 5 -ar 22050 -f mp3 $2.mp3
+
 # bin/gtts.sh API_KEY tts 'Auf zur ersten Station! Hier werden wir einen ausgeklügelten Trick der Natur genauer unter die Lupe nehmen: den Lotuseffekt. Andiesem Beispiel werden wir uns anschauen, wie wasserabweisende Oberflächen funktionieren und wo in der Natursolche Oberflächen zu finden sind. An dieser Station lernst du, wie es der Lotuspflanze gelingt, sich selbst zu reinigen, welche Vertreter in der Natur diesen Effekt noch nutzen und wie es der Mensch geschafft hat, dieses Prinzip für technische Anwendungen nutzbar zu machen. Viel Spaß bei der ersten Station! Los geht’s!'
