779a44e6c53bcd3fd34488132f80d9975a091866
Thomas K <thomas.katzlberger@locandy.com>
2020-01-23 20:55:54 +0100

    Script to use Google TTS with API-Key

---
+++ bin/gtts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# $1 API-Key
+# $2 filename without extension
+# $3 "I say this"
+
+curl -H "X-Goog-Api-Key: $1" \
+          -H "Content-Type: application/json; charset=utf-8" \
+          --data "{
+            'input':{
+              'text':'$3'
+            },
+            'voice':{
+              'languageCode':'de-de',
+              'name':'de-DE-Wavenet-B',
+              'ssmlGender':'MALE'
+            },
+            'audioConfig':{
+              'audioEncoding':'LINEAR16'
+            }
+          }" "https://texttospeech.googleapis.com/v1/text:synthesize" > $2.json
+
+jq -r .audioContent $2.json > $2.base64
+          
+base64 -D -i $2.base64 -o $2.wav
+
+rm $2.base64
+
+# bin/gtts.sh API_KEY tts 'Auf zur ersten Station! Hier werden wir einen ausgeklügelten Trick der Natur genauer unter die Lupe nehmen: den Lotuseffekt. Andiesem Beispiel werden wir uns anschauen, wie wasserabweisende Oberflächen funktionieren und wo in der Natursolche Oberflächen zu finden sind. An dieser Station lernst du, wie es der Lotuspflanze gelingt, sich selbst zu reinigen, welche Vertreter in der Natur diesen Effekt noch nutzen und wie es der Mensch geschafft hat, dieses Prinzip für technische Anwendungen nutzbar zu machen. Viel Spaß bei der ersten Station! Los geht’s!'
